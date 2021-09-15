90bc241a99047c897463cdb474ebd0572244a196
Seokju Hong <iamseokjuhong@gmail.com>
2021-09-15 12:31:55 +0900

    월요일 수업, 최대 공약수

--- java_study/src/Main.java
+++ java_study/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("두수의 최대공약수");
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("> ");
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        sc.close();
+
+        int gcd = 1;
+        for(int i=2; i<=N; i++) {
+            while(M%i==0 && N%i==0) {
+                gcd*=i;
+                M = M/i;
+                N = N/i;
+            }
+        }
+        System.out.println(gcd);
     }
 }
