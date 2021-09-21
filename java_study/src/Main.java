9bc00f2d197677fe830bd99ebd6413ee01ee2ab9
Seokju Hong <iamseokjuhong@gmail.com>
2021-09-21 15:27:24 +0900

    수요일 수업, 유클리드

--- java_study/src/Main.java
+++ java_study/src/Main.java
@@ -1,23 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("두수의 최대공약수");
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("> ");
-        int M = sc.nextInt();
-        int N = sc.nextInt();
-        sc.close();
+    public static int gcd(List<Integer> list) {
 
-        int gcd = 1;
-        for(int i=2; i<=N; i++) {
-            while(M%i==0 && N%i==0) {
-                gcd*=i;
-                M = M/i;
-                N = N/i;
+        while (list.size() >= 2) {
+            int gcd = 1;
+            int N = list.get(0);
+            int M = list.get(1);
+            for(int i=2; i<=N; i++) {
+                while(M%i==0 && N%i==0) {
+                    gcd*=i;
+                    M = M/i;
+                    N = N/i;
+                }
             }
+            list.remove(0); list.remove(0);
+            list.add(gcd);
         }
-        System.out.println(gcd);
+        return list.get(0);
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<Integer> list = new ArrayList<>();
+//        System.out.print("> ");
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        String[] line = bf.readLine().split(" ");
+//
+//        for (String s : line) {
+//            list.add(Integer.parseInt(s));
+//        }
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for(int i=0; i<N; i++) {
+            list.add(scanner.nextInt());
+        }
+
+        System.out.println(gcd(list));
+
     }
 }
