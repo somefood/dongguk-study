c1e80b64a4a6db19caa822fc1a2aa3d9de3441eb
Seokju Hong <iamseokjuhong@gmail.com>
2021-09-21 17:27:07 +0900

    월요일 수업, 비트연산

--- java_study/src/Main.java
+++ java_study/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("십진수 -> 이진수 변환");
 
+        System.out.print("> ");
+        Scanner sc = new Scanner(System.in);
+        int dec = sc.nextInt();
+        sc.close();
+
+        byte[] bin = new byte[100];
+        int cnt = 0;
+        while(dec != 0) {
+            bin[cnt++] = (byte)(dec%2);
+            dec = dec/2;
+        }
+        for(int i=0; i<cnt; i++) {
+            System.out.print(bin[cnt-1-i]);
+        }
     }
 }
