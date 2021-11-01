93d9a44840e997eb1e380321b8aba60156656200
Seokju Hong <iamseokjuhong@gmail.com>
2021-11-01 12:57:30 +0900

    수요일, 실습

--- java_study/src/Main.java
+++ java_study/src/Main.java
@@ -1,5 +1,42 @@
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class Main extends JFrame {
+
+    Main() {
+        Color[] colors = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, Color.PINK, Color.magenta, Color.LIGHT_GRAY, Color.CYAN, Color.GRAY};
+        setTitle("실습");
+
+        Container c = getContentPane();
+
+        c.setLayout(null);
+
+        for (int i=0; i<10; i++) {
+            JButton bt = new JButton(Integer.toString(i));
+            bt.setLocation(i * 80, 0);
+            bt.setSize(80, 300);
+            bt.setBackground(colors[i]);
+
+            bt.addActionListener(new MyActionLi());
+
+            c.add(bt);
+        }
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 300);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
+
+class MyActionLi implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton b = (JButton) e.getSource();
+        b.setBackground(Color.WHITE);
     }
 }
